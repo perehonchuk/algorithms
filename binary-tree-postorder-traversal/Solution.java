f2496e3e4d08eed37c750e5c811951457e2572ff
roman.peregonchuk <rperegonchuk@playtika.com>
2018-04-15 14:32:11 +0300

    leetcode.com/problems/binary-tree-postorder-traversal

---
+++ binary-tree-postorder-traversal/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.addFirst(node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return result;
+    }
+}
